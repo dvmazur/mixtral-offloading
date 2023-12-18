d3e30be8869c8323fb947bd3590ee46c9a52d8cd
Denis Mazur <denismazur8@gmail.com>
2023-12-18 22:20:19 +0300

    Update sparse_moe_block.py

--- sparse_moe_block.py
+++ sparse_moe_block.py
@@ -68,7 +68,7 @@ class MixtralSparseMoeBlock(nn.Module):
             # the current expert. We need to make sure to multiply the output hidden
             # states by `routing_weights` on the corresponding tokens (top-1 and top-2)
             current_state = hidden_states[None, top_x_list].reshape(-1, hidden_dim)
-            current_hidden_states = expert_layer(current_state, 1.0) * routing_weights[top_x_list, idx_list, None]
+            current_hidden_states = expert_layer(current_state) * routing_weights[top_x_list, idx_list, None]
 
             # However `index_add_` only support torch tensors for indexing so we'll use
             # the `top_x` tensor here.
