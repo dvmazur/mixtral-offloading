77ad58f6178f879b2267e07deae5195f6119595e
lavawolfiee <43727641+lavawolfiee@users.noreply.github.com>
2023-12-26 22:07:29 +0300

    Added 3-bit to int32 packing and unpacking

--- src/packing.py
+++ src/packing.py
@@ -6,12 +6,6 @@ class PackedTensor(torch.Tensor):
     def __init__(self, t: torch.Tensor):
         self = t
 
-    def __getattr__(self, *args, **kwargs):
-        return super().__getattribute__(*args, **kwargs)
-
-    def __getitem__(self, *args, **kwargs):
-        return super().__getitem__(*args, **kwargs)
-
 # 4 bit to uint8
 def pack_4bit_u8_common(W_q: torch.Tensor):
     height = W_q.size(0)
@@ -64,10 +58,56 @@ def unpack_2bit_u8_universal(W_q: torch.Tensor):
     else:
         return BitPack.unpack_2bit_u8(W_q)
 
+# 3 bit to int32
+def pack_3bit_i32_common(W_q: torch.Tensor):
+    height = W_q.size(0)
+    
+    # rounding height to nearest 10, because i32 can fit 10 3-bit integers
+    rem = height % 10
+    if rem == 0:
+        rem = 10
+    
+    new_height = (height + 10 - 1) // 10
+    p = torch.zeros((new_height,) + W_q.shape[1:], device=W_q.device, dtype=torch.int32)
+    
+    for i in range(10):
+        if i < rem:
+            p |= W_q[i::10, ...].to(torch.int32) << (3 * (9 - i))
+        else:
+            p[:new_height - 1, ...] |= W_q[i::10, ...].to(torch.int32) << (3 * (9 - i))
+    
+    assert p.dtype == torch.int32
+
+    return PackedTensor(p)
+
+def unpack_3bit_i32_common(W_q: torch.Tensor):
+    """
+    There may be spare rows after unpacking (height is rounded to nearest multiple of 10)
+    """
+    
+    assert W_q.dtype == torch.int32
+    height = W_q.size(0)
+    
+    result = torch.empty([10 * height] + list(W_q.shape[1:]),
+                         dtype=torch.uint8, device=W_q.device)
+    
+    for i in range(10):
+        result[i::10, ...] = (W_q >> (3 * (9 - i))) & 0b111
+
+    return result
+
+def unpack_3bit_i32_universal(W_q: torch.Tensor):
+    if isinstance(W_q, PackedTensor):
+        return unpack_3bit_i32_common(W_q)
+    else:
+        return BitPack.unpack_3bit_32(W_q)
+
 def patch_packing():
     Quantizer.pack['4bit_u8'] = pack_4bit_u8_common
     Quantizer.unpack['4bit_u8'] = unpack_4bit_u8_universal
     Quantizer.pack['2bit_u8'] = pack_2bit_u8_common
     Quantizer.unpack['2bit_u8'] = unpack_2bit_u8_universal
+    Quantizer.pack['3bit_32'] = pack_3bit_i32_common
+    Quantizer.unpack['3bit_32'] = unpack_3bit_i32_universal
 
 patch_packing()
