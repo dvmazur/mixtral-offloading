1c5b463bfa3f9628fc4188f4b6b1de2bb116efb0
Denis Mazur <denismazur8@gmail.com>
2023-12-29 21:52:37 +0300

    quant config

--- src/build_model.py
+++ src/build_model.py
@@ -2,20 +2,20 @@ import os
 import json
 from functools import cache
 from dataclasses import dataclass
+import typing as tp
 
 import torch
 from torch import nn
 
 from transformers import AutoConfig
-from transformers.models.mixtral import MixtralForCausalLM
-
+from transformers.models.mixtral import MixtralForCausalLM, MixtralConfig
 
 from safetensors.torch import load_file
 
 from torch import nn
 from tqdm.auto import trange
 
-from hqq.core.quantize import BaseQuantizeConfig, HQQLinear
+from hqq.core.quantize import BaseQuantizeConfig
 
 from .expert_cache import ExpertCache
 from .expert_wrapper import MixtralExpertWrapper
@@ -27,11 +27,34 @@ from .custom_layers import (
 from .utils import with_default_dtype
 
 
-def replace_attn_layers(model, config, device):
-    attn_params = BaseQuantizeConfig(
-        nbits=4, group_size=64, quant_zero=True, quant_scale=True
-    )
-    attn_params["scale_quant_params"]["group_size"] = 256
+@dataclass(frozen=True)
+class OffloadConfig:
+    main_size: int
+    offload_size: int
+    buffer_size: int
+    offload_per_layer: int
+
+
+@dataclass(frozen=True)
+class QuantConfig:
+    ffn_config: BaseQuantizeConfig
+    attn_config: BaseQuantizeConfig
+
+    @cache
+    def get_ffn_metas(self, hidden_dim: int, ffn_dim: int) -> tuple[tp.Any, tp.Any]:
+        return (
+            HQQLinearTritonSavable.get_hqq_meta((hidden_dim, ffn_dim), self.quant_config),
+            HQQLinearTritonSavable.get_hqq_meta((ffn_dim, hidden_dim), self.quant_config),
+        )
+
+
+def replace_attn_layers(
+    model: MixtralForCausalLM,
+    config: MixtralConfig,
+    quant_config: QuantConfig,
+    device: torch.device,
+) -> None:
+    attn_quant_config = quant_config.attn_config
 
     hidden_size = config.hidden_size
     num_heads = config.num_attention_heads
@@ -46,7 +69,7 @@ def replace_attn_layers(model, config, device):
     ]
 
     shape_to_meta = {
-        shape: HQQLinearTritonSavable.get_hqq_meta(shape, attn_params)
+        shape: HQQLinearTritonSavable.get_hqq_meta(shape, attn_quant_config)
         for shape in shapes
     }
 
@@ -55,7 +78,6 @@ def replace_attn_layers(model, config, device):
         layer = HQQLinearTritonSavable(None, quant_config, meta=meta)
         return layer
 
-    
     for layer in model.model.layers:
         layer.block_sparse_moe.gate = nn.Linear(
             config.hidden_size,
@@ -66,16 +88,16 @@ def replace_attn_layers(model, config, device):
         )
 
         layer.self_attn.q_proj = patch_fct_hqq(
-            (hidden_size, num_heads * head_dim), attn_params
+            (hidden_size, num_heads * head_dim), attn_quant_config
         )
         layer.self_attn.k_proj = patch_fct_hqq(
-            (hidden_size, num_key_value_heads * head_dim), attn_params
+            (hidden_size, num_key_value_heads * head_dim), attn_quant_config
         )
         layer.self_attn.v_proj = patch_fct_hqq(
-            (hidden_size, num_key_value_heads * head_dim), attn_params
+            (hidden_size, num_key_value_heads * head_dim), attn_quant_config
         )
         layer.self_attn.o_proj = patch_fct_hqq(
-            (hidden_size, num_heads * head_dim), attn_params
+            (hidden_size, num_heads * head_dim), attn_quant_config
         )
 
 
@@ -94,44 +116,38 @@ def get_default_ffn_quant_config(ffn_dim: int = 14336, hidden_dim: int = 4096):
     return quant_config, meta1, meta2
 
 
-def make_empty_expert(config):
-    quant_config, meta1, meta2 = get_default_ffn_quant_config()
+def make_empty_expert(model_config: MixtralConfig, quant_config: QuantConfig) -> MixtralBLockSparseTop2MLP_HQQ:
+    meta1, meta2 = quant_config.get_ffn_metas()
     return MixtralBLockSparseTop2MLP_HQQ(
-        config,
-        quant_config,
+        model_config,
+        quant_config.ffn_config,
         meta1,
         meta2,
     )
 
 
 def make_and_load_expert_wrapper(
-    config,
-    states_dir,
-    layer_idx,
-    expert_idx,
-    device,
-):
+    config: MixtralConfig,
+    quant_config: QuantConfig,
+    states_dir: str,
+    expert_uid: tuple[int, int],
+    device: torch.device,
+) -> MixtralExpertWrapper:
+    layer_idx, expert_idx = expert_uid
+
     index_path = os.path.join(states_dir, "model.safetensors.index.json")
     with open(index_path) as f:
         module_idx = f"model.layers.{layer_idx}.block_sparse_moe.experts.{expert_idx}"
         state_fpath = json.load(f)["weight_map"][f"{module_idx}.w1.W_q"]
 
     state_dict = load_file(os.path.join(states_dir, state_fpath), device=str(device))
-    expert = make_empty_expert(config)
+    expert = make_empty_expert(config, quant_config)
     expert.load_state_dict(state_dict, strict=True)
 
     return MixtralExpertWrapper(expert, device)
 
 
-def quantize_expert_inplace(expert, quant_config):
-    expert.w1 = HQQLinear(expert.w1, quant_config, del_orig=True)
-    expert.w2 = HQQLinear(expert.w2, quant_config, del_orig=True)
-    expert.w3 = HQQLinear(expert.w3, quant_config, del_orig=True)
-    expert = expert.cuda()
-    return expert
-
-
-def load_00_expert_state_dict(states_dir, device):
+def load_00_expert_state_dict(states_dir: str, device: torch.device):
     index_path = os.path.join(states_dir, "model.safetensors.index.json")
     with open(index_path) as f:
         module_idx = f"model.layers.0.block_sparse_moe.experts.0"
@@ -139,27 +155,19 @@ def load_00_expert_state_dict(states_dir, device):
     return load_file(os.path.join(states_dir, state_fpath), device=str(device))
 
 
-@dataclass
-class OffloadConfig:
-    main_size: int
-    offload_size: int
-    buffer_size: int
-    offload_per_layer: int
-
-
-@dataclass
-class QuantConfig:
-    ...
-
-
-def build_model(device: torch.device, offload_config: OffloadConfig, state_path: str):
+def build_model(
+    device: torch.device,
+    quant_config: QuantConfig,
+    offload_config: OffloadConfig,
+    state_path: str,
+):
     model_name = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
     state_dict_00 = load_00_expert_state_dict(state_path, device)
 
     def _make_module():
         config = AutoConfig.from_pretrained(model_name)
-        expert = make_empty_expert(config)
+        expert = make_empty_expert(config, quant_config)
         expert.load_state_dict(state_dict_00)
         return MixtralExpertWrapper(expert, device=device)
 
@@ -172,8 +180,9 @@ def build_model(device: torch.device, offload_config: OffloadConfig, state_path:
                 device_map=device,
             ),
         )
+
     model_config = AutoConfig.from_pretrained(model_name)
-    replace_attn_layers(model, model_config, device)
+    replace_attn_layers(model, quant_config.attn_config, model_config, device)
     state_index_path = os.path.join(state_path, "model.safetensors.index.json")
     with open(state_index_path) as f:
         weight_map = json.load(f)["weight_map"]
@@ -204,9 +213,9 @@ def build_model(device: torch.device, offload_config: OffloadConfig, state_path:
 
             expert_wrapper = make_and_load_expert_wrapper(
                 config=model_config,
+                quant_config=quant_config,
                 states_dir=state_path,
-                layer_idx=layer_idx,
-                expert_idx=expert_idx,
+                expert_uid=(layer_idx, expert_idx),
                 device=device,
             )
 
